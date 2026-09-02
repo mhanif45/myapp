8afa8a0a76428168f52b8b89f7bed62dc5d6ee4f
hanif040 <hanif11255141@gmail.com>
2026-09-02 06:06:20 +0000

    Initial commit - hello world app

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Hanif\'s CI/CD app! Version 1.0');
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
